88fcc3e65532abd3c6b0dcd20bdc8f0d2068d0b1
Pearl Sinha <89981403+pear-sinha@users.noreply.github.com>
2022-04-09 16:51:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(400, 400);
+    canvas.center();
+}
+
+function draw(){
+    background("yellow");
+   // text(mouseX + "," + mouseY, mouseX, mouseY);
+   // circle()
+}
